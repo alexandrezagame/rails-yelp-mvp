8cbfe4eeec4cd572fb8a4b9d5a78d227460ed051
Alexandre Zagame <zagamealexandre@gmail.com>
2020-03-11 16:12:41 +0100

    rails yelp app

---
+++ app/models/restaurant.rb
@@ -0,0 +1,11 @@
+class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  has_many :reviews, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :phone_number, uniqueness: true, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+
+
+end
